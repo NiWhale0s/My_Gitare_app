9fdd12cd5904fbb206610f70e3925f24d8346705
Ni_Whale_0ss <tikh-na2003@mail.ru>
2025-08-25 19:40:49 +0700

    new_commit
    add new songs, the scroll speed has been changed

--- MyApplication/app/src/main/java/com/example/myapplication/SongDetailsActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SongDetailsActivity.kt
@@ -12,11 +12,13 @@ class SongDetailsActivity : AppCompatActivity() {
     private lateinit var scrollView: ScrollView
     private lateinit var toggleButton: Button
     private lateinit var changeSpeedButton: Button
+    private lateinit var currentSpeedTextView: TextView
     private var isAutoScrolling = false
     private val handler = Handler()
 
     private var scrollSpeed = 1
     private var delay = 50L
+    private var initialDelay = 7500L // Задержка в начале (2 секунды)
 
     private val runnable = object : Runnable {
         override fun run() {
@@ -35,7 +37,7 @@ class SongDetailsActivity : AppCompatActivity() {
         val artist = intent.getStringExtra("artist")
         val fullText = intent.getStringExtra("fullText")
         val chords = intent.getStringExtra("chords")
-        val battle  = intent.getStringExtra("battle")
+        val battle = intent.getStringExtra("battle")
 
         findViewById<TextView>(R.id.detailsTitle).text = title
         findViewById<TextView>(R.id.detailsArtist).text = artist
@@ -46,6 +48,7 @@ class SongDetailsActivity : AppCompatActivity() {
         scrollView = findViewById(R.id.detailsScrollView)
         toggleButton = findViewById(R.id.toggleAutoScrollButton)
         changeSpeedButton = findViewById(R.id.changeSpeedButton)
+        currentSpeedTextView = findViewById(R.id.currentSpeedTextView)
         toggleButton.setOnClickListener {
             toggleAutoScroll()
         }
@@ -57,25 +60,32 @@ class SongDetailsActivity : AppCompatActivity() {
     private fun toggleAutoScroll() {
         isAutoScrolling = !isAutoScrolling
         if (isAutoScrolling) {
-            handler.post(runnable)
-            toggleButton.text = "Stop Auto Scroll"
+            handler.postDelayed(runnable, initialDelay) // Добавляем задержку в начале
+            toggleButton.text = "Остановить прокрутку"
         } else {
             handler.removeCallbacks(runnable)
-            toggleButton.text = "Start Auto Scroll"
+            toggleButton.text = "Начать прокрутку"
         }
     }
 
     private var currentSpeedIndex = 0
-    private val speeds = listOf(1 to 50L, 2 to 30L, 3 to 20L)
+    private val speeds = listOf(
+        1 to 225L, // Медленная скорость
+        2 to 200L, // Средняя скорость
+        3 to 175L, // Быстрая скорость
+        4 to 150L, // Очень быстрая скорость
+        5 to 125L   // Сверхбыстрая скорость
+    )
 
     private fun changeScrollSpeed() {
         val (speed, delay) = speeds[currentSpeedIndex]
         scrollSpeed = speed
         this.delay = delay
         currentSpeedIndex = (currentSpeedIndex + 1) % speeds.size
+        currentSpeedTextView.text = "Скорость: $scrollSpeed"
         if (isAutoScrolling) {
             handler.removeCallbacks(runnable)
-            handler.post(runnable)
+            handler.postDelayed(runnable, initialDelay) // Добавляем задержку в начале
         }
     }
 }
